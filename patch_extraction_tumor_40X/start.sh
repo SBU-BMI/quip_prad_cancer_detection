0c0ec5aaaa79d0f784cc5a588123059b0bd39f9e
hansbu <32404085+hansbu@users.noreply.github.com>
2019-07-05 12:18:19 -0400

    reduce threads of patch_extraction

--- patch_extraction_tumor_40X/start.sh
+++ patch_extraction_tumor_40X/start.sh
@@ -2,14 +2,10 @@
 
 source ../conf/variables.sh
 
-nohup bash save_svs_to_tiles.sh 0 8 &> ${LOG_OUTPUT_FOLDER}/log.save_svs_to_tiles.thread_0.txt &
-nohup bash save_svs_to_tiles.sh 1 8 &> ${LOG_OUTPUT_FOLDER}/log.save_svs_to_tiles.thread_1.txt &
-nohup bash save_svs_to_tiles.sh 2 8 &> ${LOG_OUTPUT_FOLDER}/log.save_svs_to_tiles.thread_2.txt &
-nohup bash save_svs_to_tiles.sh 3 8 &> ${LOG_OUTPUT_FOLDER}/log.save_svs_to_tiles.thread_3.txt &
-nohup bash save_svs_to_tiles.sh 4 8 &> ${LOG_OUTPUT_FOLDER}/log.save_svs_to_tiles.thread_4.txt &
-nohup bash save_svs_to_tiles.sh 5 8 &> ${LOG_OUTPUT_FOLDER}/log.save_svs_to_tiles.thread_5.txt &
-nohup bash save_svs_to_tiles.sh 6 8 &> ${LOG_OUTPUT_FOLDER}/log.save_svs_to_tiles.thread_6.txt &
-nohup bash save_svs_to_tiles.sh 7 8 &> ${LOG_OUTPUT_FOLDER}/log.save_svs_to_tiles.thread_7.txt &
+nohup bash save_svs_to_tiles.sh 0 4 &> ${LOG_OUTPUT_FOLDER}/log.save_svs_to_tiles.thread_0.txt &
+nohup bash save_svs_to_tiles.sh 1 4 &> ${LOG_OUTPUT_FOLDER}/log.save_svs_to_tiles.thread_1.txt &
+nohup bash save_svs_to_tiles.sh 2 4 &> ${LOG_OUTPUT_FOLDER}/log.save_svs_to_tiles.thread_2.txt &
+nohup bash save_svs_to_tiles.sh 3 4 &> ${LOG_OUTPUT_FOLDER}/log.save_svs_to_tiles.thread_3.txt &
 wait
 
 #python -u back_ground_filter_all_folders.py ${PATCH_PATH} ${SVS_INPUT_PATH}
