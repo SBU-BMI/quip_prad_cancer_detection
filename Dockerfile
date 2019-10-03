b7fb9f854bdb3d96e4f072f1a918c901ad7fb20c
Tahsin Kurc <tahsin.kurc@stonybrook.edu>
2019-10-03 08:21:38 -0400

    Changes to library versions.

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,35 @@
 FROM pytorch/pytorch:0.4.1-cuda9-cudnn7-devel 
 MAINTAINER Tahsin Kurc
 
-RUN		apt-get -y update && \
-		apt-get install --yes python3-openslide wget zip libgl1-mesa-glx libgl1-mesa-dev && \
-		pip install --upgrade pip && \
-		conda install --yes scikit-learn && \
-		pip install Pillow pymongo && \
-		pip install torchvision==0.2.1 && \
-		pip install openslide-python && \
-		conda install --yes -c conda-forge opencv
+RUN	apt-get -y update && \
+	apt-get install -y libsm6 \ 
+		libxext6 \
+		libxrender-dev \
+		libglib2.0-0 \
+		python3-openslide \
+		wget \
+		zip \
+		libgl1-mesa-glx \
+		libgl1-mesa-dev
+
+RUN 	conda update -n base -c defaults conda && \
+	conda install --yes scikit-learn && \
+	pip install Pillow pymongo && \
+	pip install openslide-python && \
+	pip install opencv-python
+
+RUN	conda install --yes pytorch=0.4.0 torchvision=0.2.0
 
 COPY	. /root/quip_prad_cancer_detection/.
 
-RUN		chmod 0755 /root/quip_prad_cancer_detection/scripts/*
+WORKDIR /root/quip_prad_cancer_detection/models_cnn
+RUN	wget -v -O \
+	RESNET_34_prostate_trueVal_hard_train__0530_0015_0.954882634484846_1919.t7 \
+	-L https://stonybrookmedicine.box.com/shared/static/nv9xs53n9pus5kz9pzv21z97vrwvz4ek.t7 
 
-ENV	BASE_DIR="/root/quip_prad_cancer_detection"
+WORKDIR /root/quip_prad_cancer_detection/scripts
+RUN	chmod 0755 *
+
 ENV	PATH="./":$PATH
-WORKDIR	/root/quip_prad_cancer_detection/scripts
 
 CMD ["/bin/bash"]
--- conf/variables.sh
+++ conf/variables.sh
@@ -4,46 +4,47 @@
 DEFAULT_OBJ=20
 DEFAULT_MPP=0.5
 CANCER_TYPE=quip
-MONGODB_HOST=quip4.bmi.stonybrook.edu
+MONGODB_HOST=xxxx
 MONGODB_PORT=27017
+
 HEATMAP_VERSION=cancer-prad
 
 # Base directory
-BASE_DIR=/data10/shared/hanle/quip_prad_cancer_detection
+BASE_DIR=/root/quip_prad_cancer_detection
+DATA_DIR=/data
 
 # The username you want to download heatmaps from
-#USERNAME=olcabasturk@gmail.com
-USERNAME=praitayini.kanakaraj@stonybrook.edu
-USERNAME=ledinhhan2812@gmail.com
+USERNAME=abc@test.test
 
 # The list of case_ids you want to download heaetmaps from
-CASE_LIST=${BASE_DIR}/data/raw_marking_to_download_case_list/case_list.txt
+CASE_LIST=${BASE_DIR}/raw_marking_to_download_case_list/case_list.txt
 
 # Paths of data, log, input, and output
-JSON_OUTPUT_FOLDER=${BASE_DIR}/data/heatmap_jsons
-HEATMAP_TXT_OUTPUT_FOLDER=${BASE_DIR}/data/heatmap_txt
-LOG_OUTPUT_FOLDER=${BASE_DIR}/data/log
-SVS_INPUT_PATH=${BASE_DIR}/data/svs
-PATCH_PATH=${BASE_DIR}/data/patches
-PATCH_SAMPLING_LIST_PATH=${BASE_DIR}/data/patch_sample_list
-RAW_MARKINGS_PATH=${BASE_DIR}/data/raw_marking_xy
-MODIFIED_HEATMAPS_PATH=${BASE_DIR}/data/modified_heatmaps
-TUMOR_HEATMAPS_PATH=${BASE_DIR}/data/tumor_labeled_heatmaps
-TUMOR_GROUND_TRUTH=${BASE_DIR}/data/tumor_ground_truth_maps
-TUMOR_IMAGES_TO_EXTRACT=${BASE_DIR}/data/tumor_images_to_extract
-GRAYSCALE_HEATMAPS_PATH=${BASE_DIR}/data/grayscale_heatmaps
-THRESHOLDED_HEATMAPS_PATH=${BASE_DIR}/data/thresholded_heatmaps
-PATCH_FROM_HEATMAP_PATH=${BASE_DIR}/data/patches_from_heatmap
-THRESHOLD_LIST=${BASE_DIR}/data/threshold_list/threshold_list.txt
+JSON_OUTPUT_FOLDER=${DATA_DIR}/output/heatmap_jsons
+HEATMAP_TXT_OUTPUT_FOLDER=${DATA_DIR}/output/heatmap_txt
+LOG_OUTPUT_FOLDER=${DATA_DIR}/output/log
+SVS_INPUT_PATH=${DATA_DIR}/svs
+PATCH_PATH=${DATA_DIR}/patches
 
-CAE_TRAINING_DATA=${BASE_DIR}/data/training_data_cae
+PATCH_SAMPLING_LIST_PATH=${DATA_DIR}/patch_sample_list
+RAW_MARKINGS_PATH=${DATA_DIR}/raw_marking_xy
+MODIFIED_HEATMAPS_PATH=${DATA_DIR}/modified_heatmaps
+TUMOR_HEATMAPS_PATH=${DATA_DIR}/tumor_labeled_heatmaps
+TUMOR_GROUND_TRUTH=${DATA_DIR}/tumor_ground_truth_maps
+TUMOR_IMAGES_TO_EXTRACT=${DATA_DIR}/tumor_images_to_extract
+GRAYSCALE_HEATMAPS_PATH=${DATA_DIR}/grayscale_heatmaps
+THRESHOLDED_HEATMAPS_PATH=${DATA_DIR}/thresholded_heatmaps
+PATCH_FROM_HEATMAP_PATH=${DATA_DIR}/patches_from_heatmap
+THRESHOLD_LIST=${DATA_DIR}/threshold_list/threshold_list.txt
+
+CAE_TRAINING_DATA=${BASE_DIR}/training_data_cae
 CAE_TRAINING_DEVICE=gpu0
-CAE_MODEL_PATH=${BASE_DIR}/data/models_cae
-LYM_CNN_TRAINING_DATA=${BASE_DIR}/data/training_data_cnn
+CAE_MODEL_PATH=${BASE_DIR}/models_cae
+LYM_CNN_TRAINING_DATA=${BASE_DIR}/training_data_cnn
 LYM_CNN_TRAINING_DEVICE=gpu0
 LYM_CNN_PRED_DEVICE=gpu0
-LYM_NECRO_CNN_MODEL_PATH=${BASE_DIR}/data/models_cnn
-NEC_CNN_TRAINING_DATA=${BASE_DIR}/data/training_data_cnn
+LYM_NECRO_CNN_MODEL_PATH=${BASE_DIR}/models_cnn
+NEC_CNN_TRAINING_DATA=${BASE_DIR}/training_data_cnn
 NEC_CNN_TRAINING_DEVICE=gpu1
 NEC_CNN_PRED_DEVICE=gpu0
 EXTERNAL_LYM_MODEL=0
