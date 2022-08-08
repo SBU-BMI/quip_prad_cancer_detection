b71d8440eab090cb789281b33fbf89011e924fb9
tkurc <tkurc@harrier.cm.cluster>
2022-08-08 11:00:08 -0400

    Handle various filename extensions.

--- heatmap_gen_separate_classes/1_cp_heatmaps_all.sh
+++ heatmap_gen_separate_classes/1_cp_heatmaps_all.sh
@@ -8,7 +8,10 @@ PRED_VERSION=patch-level-3classes.txt
 
 DIS_FOLDER=${HEATMAP_TXT_OUTPUT_FOLDER}
 for files in ${FOLDER}/*/${PRED_VERSION}; do
-    dis=`echo ${files} | awk -F'/' '{print "prediction-"substr($(NF-1),1,length($(NF-1))-4);}'`
+    fname=`echo ${files} | awk -F '/' '{print $(NF-1)}'`;
+    fprefix="${fname%.*}"
+    dis="prediction-"$fprefix
+    # dis=`echo ${files} | awk -F'/' '{print "prediction-"substr($(NF-1),1,length($(NF-1))-4);}'`
     cp ${files} ${DIS_FOLDER}/${dis}
     echo -e "x_loc y_loc grade3 grade4+5 benign\n$(cat ${DIS_FOLDER}/${dis})" > ${DIS_FOLDER}/${dis}
 done
@@ -16,7 +19,10 @@ done
 PRED_VERSION=patch-level-color.txt
 DIS_FOLDER=${HEATMAP_TXT_OUTPUT_FOLDER}
 for files in ${FOLDER}/*/${PRED_VERSION}; do
-    dis=`echo ${files} | awk -F'/' '{print "color-"substr($(NF-1),1,length($(NF-1))-4);}'`
+    fname=`echo ${files} | awk -F '/' '{print $(NF-1)}'`;
+    fprefix="${fname%.*}"
+    dis="color-"$fprefix
+    # dis=`echo ${files} | awk -F'/' '{print "color-"substr($(NF-1),1,length($(NF-1))-4);}'`
     cp ${files} ${DIS_FOLDER}/${dis}
 done
 
